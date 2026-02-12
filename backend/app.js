c13bb4d66cfdd5517ac4d53ad40f8471338bce4e
prateekmtri <prateek1tri2@gmail.com>
2026-02-12 16:10:00 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5000;
+
+app.use(cors()); 
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Express Backend 🚀" });
